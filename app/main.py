8b451965bff4f1c95df8c1d93d51ab39da052afb
BrokenShell <webmaster@sharpdesigndigital.com>
2020-08-15 14:53:32 -0700

    adds Flask

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from Fortuna import random_value
+from flask import Flask, jsonify
+import pandas as pd
+"""
+Input  :: Text? Dropdown Selection? Numeric? 
+Output :: type: string, description, list flavors, list effects
+"""
+
+__all__ = ('random_recommendation', 'index_route', 'APP')
+
+
+def load_data():
+    return pd.read_csv('cannabis.csv').to_dict(orient='records')
+
+
+APP = Flask(__name__)
+DATA = load_data()
+
+
+def random_recommendation():
+    return random_value(DATA)
+
+
+@APP.route('/')
+def index_route():
+    return jsonify(random_recommendation())
+
+
+if __name__ == '__main__':
+    APP.run()
