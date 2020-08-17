dc457651f60fc73d78ba9f62e17f0c46430ca7fd
BrokenShell <webmaster@sharpdesigndigital.com>
2020-08-16 22:50:12 -0700

    3.5 "381" fix

--- app/data.py
+++ app/data.py
@@ -5,54 +5,121 @@ August 2020
 """
 from collections import defaultdict
 from typing import List
-from pandas import read_csv
+
 from Fortuna import random_value, FlexCat
+from pandas import read_csv
 
 
 class StrainData:
     """ Primary Data Object for MedCab """
+
     __slots__ = (
-        'df', 'data', 'effect_lookup', 'flavor_lookup', 'type_lookup',
+        'data', 'effect_lookup', 'flavor_lookup', 'type_lookup',
         'name_lookup', 'random_by_type', 'random_by_effect', 'random_by_flavor',
     )
 
     def __init__(self, filename):
-        self.df = read_csv(filename)
-        self.df['Type'] = self.df['Type'].str.title()
-        self.df['Flavors'] = self.df['Flavors'].str.replace('/', ',')
-        self.df['Strain'] = self.df['Strain'].apply(self._fix_string)
-        self.df['Description'] = self.df['Description'].apply(self._fix_string)
-        self.data = self.df.to_dict(orient='records')
-        self.effect_lookup = defaultdict(list)
-        self.flavor_lookup = defaultdict(list)
-        self.type_lookup = defaultdict(list)
-        self.name_lookup = dict()
+        # Temporary Raw Data
+        df = read_csv(filename)
+        df['Type'] = df['Type'].str.title()
+        df['Flavors'] = df['Flavors'].str.replace('/', ',')
+        df['Strain'] = df['Strain'].apply(self._fix_string)
+        df['Description'] = df['Description'].apply(self._fix_string)
+
+        # Init Lookup Tables
+        self.data = df.to_dict(orient='records')      # List[Dict {Key: Value}]
+        self.effect_lookup = defaultdict(list)        # Dict {Key: List[String]}
+        self.flavor_lookup = defaultdict(list)        # Dict {Key: List[String]}
+        self.type_lookup = defaultdict(list)          # Dict {Key: List[String]}
+        self.name_lookup = dict()                # Dict {Key: Dict {Key: Value}}
+
+        # Fill Lookup Tables
         for strain in self.data:
-            strain['Flavors'] = strain['Flavors'].split(',')
             strain['Effects'] = strain['Effects'].split(',')
-            self.type_lookup[strain['Type']].append(strain['Strain'])
             for effect in strain['Effects']:
                 self.effect_lookup[effect].append(strain['Strain'])
+
+            strain['Flavors'] = strain['Flavors'].split(',')
             for flavor in strain['Flavors']:
                 self.flavor_lookup[flavor].append(strain['Strain'])
+
+            self.type_lookup[strain['Type']].append(strain['Strain'])
             self.name_lookup[strain['Strain']] = strain
-        self.random_by_type = FlexCat(
-            {k: v for k, v in self.type_lookup.items()},
-            key_bias='truffle_shuffle', val_bias='truffle_shuffle',
-        )
-        self.random_by_effect = FlexCat(
-            {k: v for k, v in self.effect_lookup.items()},
-            key_bias='truffle_shuffle', val_bias='truffle_shuffle',
-        )
-        self.random_by_flavor = FlexCat(
-            {k: v for k, v in self.flavor_lookup.items()},
-            key_bias='truffle_shuffle', val_bias='truffle_shuffle',
-        )
+
+        # Setup Randomizers
+        self.random_by_type = FlexCat({k: v for k, v in self.type_lookup.items()})
+        self.random_by_effect = FlexCat({k: v for k, v in self.effect_lookup.items()})
+        self.random_by_flavor = FlexCat({k: v for k, v in self.flavor_lookup.items()})
+
+    def random_strain(self) -> dict:
+        """ Returns a random Strain
+        @return: dict
+        """
+        return random_value(self.data)
+
+    def effects_list(self) -> List[str]:
+        """ List of all Effects
+        @return: List[str]
+        """
+        return list(self.effect_lookup.keys())
+
+    def types_list(self) -> List[str]:
+        """ List of all Types
+        @return: List[str]
+        """
+        return list(self.type_lookup.keys())
+
+    def flavors_list(self) -> List[str]:
+        """ List of all Flavors
+        @return: List[str]
+        """
+        return list(self.flavor_lookup.keys())
+
+    def strain_by_id(self, idx: str) -> dict:
+        """ Strain lookup by Index
+        @param idx: str
+        @return: dict
+        """
+        return self.data[int(idx)]
+
+    def strain_by_name(self, name: str) -> dict:
+        """ Strain lookup by Name
+        @param name: str
+        @return: dict
+        """
+        return self.name_lookup[name]
+
+    def strains_by_type(self, strain_type: str) -> List[str]:
+        """ List of Strains of the desired Type
+        @param strain_type: str
+        @return: List[str]
+        """
+        return self.type_lookup[strain_type]
+
+    def strains_by_rating(self, rating: str) -> List[str]:
+        """ List of Strains of the desired Rating or higher
+        @param rating: str
+        @return: List[str]
+        """
+        return self.data
+
+    def strains_by_effect(self, effect: str) -> List[str]:
+        """ List of Strains that produce the desired Effect
+        @param effect: str
+        @return: List[str]
+        """
+        return self.effect_lookup[effect]
+
+    def strains_by_flavor(self, flavor: str) -> List[str]:
+        """ List of Strains that have the desired Flavor
+        @param flavor: str
+        @return: List[str]
+        """
+        return self.flavor_lookup[flavor]
 
     @staticmethod
     def _fix_string(string: str) -> str:
-        """ Unicode Field Medic Solution ...
-
+        """ Unicode Field Medic Solution ... Let's not talk about this.
         @param string: str
         @return: str
         """
@@ -77,79 +144,3 @@ class StrainData:
         ).replace(
             '\u0101', 'a',
         )
-
-    def random_strain(self) -> dict:
-        """ Returns a random Strain
-
-        @return: dict
-        """
-        return random_value(self.data)
-
-    def effects_list(self) -> List[str]:
-        """ List of all Effects
-
-        @return: List[str]
-        """
-        return list(self.effect_lookup.keys())
-
-    def types_list(self) -> List[str]:
-        """ List of all Types
-
-        @return: List[str]
-        """
-        return list(map(lambda s: s.title(), self.type_lookup.keys()))
-
-    def flavors_list(self) -> List[str]:
-        """ List of all Flavors
-
-        @return: List[str]
-        """
-        return list(self.flavor_lookup.keys())
-
-    def strain_by_id(self, idx: str) -> dict:
-        """ Strain lookup by Index
-
-        @param idx: str
-        @return: dict
-        """
-        return self.data[int(idx)]
-
-    def strain_by_name(self, name: str) -> dict:
-        """ Strain lookup by Name
-
-        @param name: str
-        @return: dict
-        """
-        return self.name_lookup[name]
-
-    def strains_by_type(self, strain_type: str) -> List[str]:
-        """ List of Strains of the desired Type
-
-        @param strain_type: str
-        @return: List[str]
-        """
-        return self.type_lookup[strain_type.title()]
-
-    def strains_by_rating(self, rating: str) -> List[str]:
-        """ List of Strains of the desired Rating or higher
-
-        @param rating: str
-        @return: List[str]
-        """
-        return list()
-
-    def strains_by_effect(self, effect: str) -> List[str]:
-        """ List of Strains that produce the desired Effect
-
-        @param effect: str
-        @return: List[str]
-        """
-        return self.effect_lookup[effect]
-
-    def strains_by_flavor(self, flavor: str) -> List[str]:
-        """ List of Strains that have the desired Flavor
-
-        @param flavor: str
-        @return: List[str]
-        """
-        return self.flavor_lookup[flavor]
--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ from app.data import StrainData
 
 """ One Ring to rule them all, and in the darkness - bind them! """
 Ring = Flask(__name__)
-Ring.data = StrainData(filename='data/cannabis.csv')
+Ring.data = StrainData(filename='data/cannabis_new.csv')
 
 
 @Ring.route('/')
 @Ring.route('/random')
-@Ring.route('/index.html')
 def index():
     """ Default Endpoint - Returns a random Strain
     @return: JSON object
@@ -17,7 +16,7 @@ def index():
     return jsonify(Ring.data.random_strain())
 
 
-@Ring.route('/strain-by-id/<idx>', methods=['GET'])
+@Ring.route('/strain-by-id/<idx>')
 def strain_by_id(idx: str):
     """ Strain Details, Id Lookup Table
     @param idx: String, index of desired strain
@@ -26,7 +25,7 @@ def strain_by_id(idx: str):
     return jsonify(Ring.data.strain_by_id(idx))
 
 
-@Ring.route('/strain-by-name/<name>', methods=['GET'])
+@Ring.route('/strain-by-name/<name>')
 def strain_by_name(name: str):
     """ Strain Details, Name Lookup Table.
     @param name: String
@@ -66,7 +65,7 @@ def strain_flavors():
     return jsonify(Ring.data.flavors_list())
 
 
-@Ring.route('/strains-by-effect/<effect>', methods=['GET'])
+@Ring.route('/strains-by-effect/<effect>')
 def strains_by_effect(effect):
     """ Returns a list of Strain names.
     @param effect: str
@@ -75,7 +74,7 @@ def strains_by_effect(effect):
     return jsonify(Ring.data.strains_by_effect(effect.title()))
 
 
-@Ring.route('/strains-by-flavor/<flavor>', methods=['GET'])
+@Ring.route('/strains-by-flavor/<flavor>')
 def strains_by_flavor(flavor):
     """ Returns a list of Strain names.
     @param flavor: str
@@ -84,73 +83,73 @@ def strains_by_flavor(flavor):
     return jsonify(Ring.data.strains_by_flavor(flavor.title()))
 
 
-@Ring.route('/strains-by-type/<strain_type>', methods=['GET'])
+@Ring.route('/strains-by-type/<strain_type>')
 def strains_by_type(strain_type):
     """ Returns a list of Strain names.
     @param strain_type: str
     @return: JSON Obj: List[str]
     """
-    return jsonify(Ring.data.strains_by_type(strain_type.lower()))
+    return jsonify(Ring.data.strains_by_type(strain_type.title()))
 
 
-@Ring.route('/random-by-type/<strain_type>', methods=['GET'])
+@Ring.route('/random-by-type/<strain_type>')
 def random_by_type(strain_type):
     """ Random Strain by Type
-
     @param strain_type: str
     @return: JSON Obj
     """
     return jsonify(
-        Ring.data.strain_by_name(Ring.data.random_by_type(strain_type.title())))
+        Ring.data.strain_by_name(Ring.data.random_by_type(strain_type.title()))
+    )
 
 
-@Ring.route('/random-by-effect/<effect>', methods=['GET'])
+@Ring.route('/random-by-effect/<effect>')
 def random_by_effect(effect):
     """ Random Strain by Effect
-
     @param effect: str
     @return: JSON Obj
     """
     return jsonify(
-        Ring.data.strain_by_name(Ring.data.random_by_effect(effect.title())))
+        Ring.data.strain_by_name(Ring.data.random_by_effect(effect.title()))
+    )
 
 
-@Ring.route('/random-by-flavor/<flavor>', methods=['GET'])
+@Ring.route('/random-by-flavor/<flavor>')
 def random_by_flavor(flavor):
     """ Random Strain by Flavor
-
     @param flavor: str
     @return: JSON Obj
     """
     return jsonify(
-        Ring.data.strain_by_name(Ring.data.random_by_flavor(flavor.title())))
+        Ring.data.strain_by_name(Ring.data.random_by_flavor(flavor.title()))
+    )
 
 
 if __name__ == '__main__':
     """
     # Local Routes:
     
-    # Random Strain Object
-    http://127.0.0.1:5000/
+    # Random Strain: Object
+    http://127.0.0.1:5000/random
     http://127.0.0.1:5000/random-by-type/sativa
     http://127.0.0.1:5000/random-by-effect/happy
     http://127.0.0.1:5000/random-by-flavor/sweet
     
-    # List of all Strain Objects
-    http://127.0.0.1:5000/strains
-
-    # List of Names
+    # Strain Object Lookup Tables: Object
+    http://127.0.0.1:5000/strain-by-id/381
+    http://127.0.0.1:5000/strain-by-name/Fruit-Loops
+    
+    # List of Names: List[String]
     http://127.0.0.1:5000/types
     http://127.0.0.1:5000/effects
     http://127.0.0.1:5000/flavors
     
-    # Strain Object Lookup Tables
-    http://127.0.0.1:5000/strain-by-id/381
-    http://127.0.0.1:5000/strain-by-name/Fruit-Loops
-    
-    # List of Names:
     http://127.0.0.1:5000/strains-by-effect/happy
     http://127.0.0.1:5000/strains-by-flavor/sweet
     http://127.0.0.1:5000/strains-by-type/sativa
+
+    # List of all Strains: Object {Name: Strain}
+    http://127.0.0.1:5000/strains
+    
     """
     Ring.run(debug=True)
