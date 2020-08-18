8c2c39409a416e72ccf6744d9db9735e57d13564
BrokenShell <webmaster@sharpdesigndigital.com>
2020-08-18 15:23:26 -0700

    5.2 adds CORS support

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ Data Science Unit 4 Build Week
 Robert Sharp
 August 2020
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
 from app.data import StrainData
 
 
@@ -149,6 +149,29 @@ def nearest(name: str):
         ])
     else:
         return jsonify(f"Unknown Strain: {clean_name}")
+
+
+@Ring.before_request
+def before_request():
+    """ CORS preflight, required for off-server access """
+
+    def _build_cors_prelight_response():
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response
+
+    if request.method == "OPTIONS":
+        return _build_cors_prelight_response()
+
+
+@Ring.after_request
+def after_request(response):
+    """ CORS headers, required for off-server access """
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 if __name__ == '__main__':
